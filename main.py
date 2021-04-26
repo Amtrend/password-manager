f1fd6cdece1254b33cc5e5e787b23a15b3119b8d
Amtrend <alexandermaltsev@bk.ru>
2021-04-26 20:27:59 +0300

    Added password search, saving to json and catching exceptions and errors

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 def generate_password():
@@ -24,20 +25,55 @@ def generate_password():
 
 
 def save():
-    website = website_entry.get()
-    email= email_entry.get()
+    website_before = website_entry.get()
+    website = website_before.lower()
+    email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are details entered: \nEmail: {email}"
-                                                              f" \nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        # is_ok = messagebox.askokcancel(title=website, message=f"These are details entered: \nEmail: {email}"
+        #                                                       f" \nPassword: {password} \nIs it ok to save?")
+        # if is_ok:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+                # data_file.write(f"{website} | {email} | {password}\n")
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+def find_password():
+    website_before = website_entry.get()
+    website = website_before.lower()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 
 window = Tk()
@@ -58,8 +94,8 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:", bg="white")
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=53)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=34)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
 email_entry = Entry(width=53)
@@ -69,10 +105,13 @@ email_entry.insert(0, "my_email@my_domain.com")
 password_entry = Entry(width=34)
 password_entry.grid(row=3, column=1)
 
-generate_password_button = Button(text="Generate Password", bg="white", command=generate_password)
+generate_password_button = Button(text="Generate Password", bg="white", width=15, command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=45, bg="white", command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text="Search", bg="white", width=15, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 Test development of the simple program with password manager.
 
 * The program allows you to generate random complex passwords for registration
-* The program allows you to save data for registration in the format: <site> | 
-<mail / user> | <password>
+* The program allows you to save data for registration in the format: <s> site  | 
+mail/user | password</s> JSON
 * When a password is generated, it is automatically saved to the clipboard. 
 You just need to paste in the desired area when registering.
+* The program allows you to find the saved password for the specified site (in any case).
